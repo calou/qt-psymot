aab90fa1af283c755aa567b904435d793aacb436
Sébastien Gruchet <gruchet@gmail.com>
2015-03-22 03:37:31 -0700

    Renommage de méthodes

--- app/gui/window/Home.py
+++ app/gui/window/Home.py
@@ -1,7 +1,6 @@
 # -*- coding: utf8 -*-
 
 from PyQt4 import QtGui, QtCore
-from gui.design.window.HomeDesign import *
 from gui.window.HomeSection import *
 from model.stimuli import *
 from gui.window.stimuli.DetailsWindow import DetailsWindow
@@ -21,22 +20,21 @@ class Home(QtGui.QWidget):
 
     def init_ui(self):
         testing_section = HomeSection(["Démarrer un test", "Consulter un test"], "assets/images/checklist.svg")
-        testing_section.push_buttons[0].clicked.connect(self.emit_go_to_testing)
+        testing_section.push_buttons[0].clicked.connect(self.go_to_testing)
         testing_section.push_buttons[1].clicked.connect(self.go_to_testing_details)
 
         self.gridLayout.addWidget(testing_section, 0, 0)
 
         patients_section = HomeSection(["Gérer les patients"], "assets/images/people.svg")
-        patients_section.push_buttons[0].clicked.connect(self.emit_go_to_patients)
+        patients_section.push_buttons[0].clicked.connect(self.go_to_patients)
         self.gridLayout.addWidget(patients_section, 0, 1)
         self.setLayout(self.gridLayout)
 
-
-    def emit_go_to_testing(self):
+    def go_to_testing(self):
         widget = ConfigurationWindow(self.root_widget)
         self.root_widget.replaceWindow(widget)
 
-    def emit_go_to_patients(self):
+    def go_to_patients(self):
         widget = ManagePatientWindow(self.root_widget)
         self.root_widget.replaceWindow(widget)
 
