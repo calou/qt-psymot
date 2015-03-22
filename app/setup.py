50ce6eb9f2d139142c9ee3d75454b47f07f3eeb3
Sébastien Gruchet <gruchet@gmail.com>
2015-03-22 10:23:25 -0700

    Ajout de setup.py

---
+++ app/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Psymot',
+    version='',
+    packages=['', 'db', 'gui', 'gui.design', 'gui.design.window', 'gui.window', 'gui.window.stimuli',
+              'gui.window.stimuli.design', 'gui.window.patients', 'model', 'utils'],
+    package_dir={'': 'app'},
+    url='',
+    license='',
+    author='Sébastien Gruchet',
+    author_email='gruchet@gmail.com',
+    description=''
+)
