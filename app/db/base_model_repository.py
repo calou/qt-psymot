3af69db38cd095c23de9c440921ebf7270a8ba5f
Sébastien Gruchet <gruchet@gmail.com>
2015-04-01 08:17:16 +0200

    Ordre des patients

--- app/db/base_model_repository.py
+++ app/db/base_model_repository.py
@@ -8,11 +8,10 @@ class PersonRepository(Repository):
 
     def list(self):
         session = self.get_session()
-        return session.query(Person).order_by(Person.id)
-
+        return session.query(Person).order_by(Person.last_name, Person.first_name)
 
     def search(self, search_value):
         value = "%" + search_value + "%"
         session = self.get_session()
         return session.query(Person).filter(or_(Person.first_name.like(value), Person.last_name.like(value))).order_by(
-            Person.id)
+            Person.last_name, Person.first_name)
